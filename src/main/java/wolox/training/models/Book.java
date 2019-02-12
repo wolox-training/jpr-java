8ebac4ec882b59ef6a4495f3ebec207bf8c14402
Juan Pablo Rojas <juanpablo.rojas@wolox.co>
2019-02-12 12:19:15 -0500

    added the described Book model with respective attributes and constructor. Also added getter and setter methods for the attributes

---
+++ src/main/java/wolox/training/models/Book.java
@@ -0,0 +1,90 @@
+package wolox.training.models;
+
+public class Book {
+    private String genre, author, image, title, subtitle, publisher, year, isbn;
+    private Integer pages;
+
+    public Book(String author, String image, String title, String subtitle, String publisher, String year,
+                String isbn, Integer pages) {
+        this.author = author;
+        this.image = image;
+        this.title = title;
+        this.subtitle = subtitle;
+        this.publisher = publisher;
+        this.year = year;
+        this.isbn = isbn;
+        this.pages = pages;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+}
