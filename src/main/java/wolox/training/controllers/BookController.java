840e851e48649e98b7ac0b0b5443e6e2eb879a50
Juan Pablo Rojas <juanpablo.rojas@wolox.co>
2019-02-11 22:54:11 -0500

    added missing empty line to files

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -1,5 +1,7 @@
 package wolox.training.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,5 +15,4 @@ public class BookController {
         model.addAttribute("name", name);
         return "greeting";
     }
-
 }
